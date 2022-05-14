7df0b5571e64dd42e43df075bf561a5d168cd711
0ldP0rtr3r0 <ianwheeler.tech@gmail.com>
2022-05-14 11:12:45 -0500

    Send email portion of program completed; SERVER, PORT, FROM, TO, PASS as well as authenticator strings

--- hn_news_scraper.py
+++ hn_news_scraper.py
@@ -27,4 +27,51 @@ def extract_news(url):
         #print(tag.prettify) #find all('span'.attrs={'class':'sitestr'}))
         return(cnt)
 
-    
+cnt = extract_news('https://news.ycombinator.com/')
+content += cnt
+content += ('<br>------<br>')
+content += ('<br><br>End of Message')
+
+# lets send the email
+print('Composing Email...')
+
+# update your email details
+SERVER = 'smtp.gmail.com' # "your smtp server"
+PORT = 597 # your port number
+FROM = '' # "your email id"
+TO = '' # "your to email ids" # can be a list of email ids
+PASS = '' # "your email ids password"
+
+# fp = open(file_name, 'rb')
+
+# Create a text/plain message 
+# msg = MIMEText('')
+msg = MIMEMultipart()
+
+# msg.add_header('Content-Disposition', 'attachment; filename='empty.txt')
+msg['Subject'] = 'Top News Stories HN [Automated Email]' + ' ' + str(now.day) + '-' + str(now.month) + '-' + str(now.year)
+msg['From'] = FROM
+msg['To'] = TO
+
+msg.attach(MIMEText(content, 'html'))
+# fp.close()
+
+print('Initializing Server...')
+
+server = smtplib.SMTP(SERVER, PORT)
+# server = smtplib.SMTP SSL('smtp.gmail.com', 465)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+# server.ehlo
+server.login(FROM, PASS)
+server.sendmail(FROM, TO, msg.as_string())
+
+print('Email Sent...')
+
+server.quit()
+
+
+
+
+
